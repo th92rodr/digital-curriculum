30db3813b828f3f74817fcb76e5a747c09fac007
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-12-08 09:35:31 -0300

    Update software skills section

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
-(function() {
+(function () {
   $('.skills-prog li')
     .find('.skills-bar')
-    .each(function(i) {
+    .each(function (i) {
       $(this)
         .find('.bar')
         .delay(i * 150)
         .animate(
           {
-            width:
-              $(this)
-                .parents()
-                .attr('data-percent') + '%'
+            width: $(this).parents().attr('data-percent') + '%',
           },
           1000,
           'linear',
-          function() {
+          function () {
             return $(this).css({
-              'transition-duration': '.5s'
+              'transition-duration': '.5s',
             });
-          }
+          },
         );
     });
 
-  $('.skills-soft li')
+  $('.skills_software li')
     .find('svg')
-    .each(function(i) {
+    .each(function (i) {
       var c, cbar, circle, percent, r;
       circle = $(this).children('.cbar');
       r = circle.attr('r');
       c = Math.PI * (r * 2);
-      percent = $(this)
-        .parent()
-        .data('percent');
+      percent = $(this).parent().data('percent');
       cbar = ((100 - percent) / 100) * c;
       circle.css({
         'stroke-dashoffset': c,
-        'stroke-dasharray': c
+        'stroke-dasharray': c,
       });
       circle.delay(i * 150).animate(
         {
-          strokeDashoffset: cbar
+          strokeDashoffset: cbar,
         },
         1000,
         'linear',
-        function() {
+        function () {
           return circle.css({
-            'transition-duration': '.3s'
+            'transition-duration': '.3s',
           });
-        }
+        },
       );
       $(this)
         .siblings('small')
@@ -55,14 +50,14 @@
         .delay(i * 150)
         .animate(
           {
-            Counter: percent
+            Counter: percent,
           },
           {
             duration: 1000,
-            step: function(now) {
+            step: function (now) {
               return $(this).text(Math.ceil(now) + '%');
-            }
-          }
+            },
+          },
         );
     });
 }.call(this));
